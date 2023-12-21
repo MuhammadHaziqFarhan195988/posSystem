2cd4194721a8d71c72d796dbce5703f495c5652d
Muhammad Haziq Farhan bin Mohd Lip 195988 <haziqf6@gmail.com>
2023-12-22 07:53:55 +0800

    part 20

--- admin/orders.php
+++ admin/orders.php
@@ -9,7 +9,10 @@
 
         <?php
         $query = "SELECT o.*,c.* FROM orders o, customers c WHERE c.id = o.customer_id ORDER BY o.id DESC";
-        $orders = mysqli_query($connection, $query);
+        $orders = mysqli_query($connection, $query); 
+        #query for selecting all data in the orders table but keep in mind the SQL result
+        # is far differ from what is rendered to the website, this is due to how we selectively choose 
+        # which data to render despite having the data available
         if($orders){
 
             if(mysqli_num_rows($orders) > 0){
@@ -37,8 +40,12 @@
                             <td><?= $orderItem['order_status']; ?></td>
                             <td><?= $orderItem['payment_mode']; ?></td>
                             <td>
-                                <a href="" class= "btn btn-info mb-0 px-2 btn-sm">View</a>
+                                <a href="orders-view.php?track=<?= $orderItem['tracking_no']; ?>" class= "btn btn-info mb-0 px-2 btn-sm">View</a>
                                 <a href="" class= "btn btn-primary mb-0 px-2 btn-sm">Print</a>
+                                <!-- if you notice there is a ? after the .php and that is because
+                                we want it to go to the orders-view page but only after the item that we
+                                are looking for, you can try same thing in google search bar -->
+                                
                             </td>
                         </tr>
                         <?php endforeach; ?>
